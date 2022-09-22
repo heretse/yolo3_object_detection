71f6fc2dec00d0a6761e0161bf5831720a261f18
Winston Hsieh <heretse@gmail.com>
2022-09-22 20:01:03 +0800

    Support yolov4-tiny.weights model

--- install-essentials.sh
+++ install-essentials.sh
@@ -9,3 +9,5 @@ wget -O ./cfg/yolov3.cfg https://raw.githubusercontent.com/AlexeyAB/darknet/mast
 wget -O ./cfg/yolov3-tiny.cfg https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov3-tiny.cfg
 wget -O ./models/yolov3.weights https://pjreddie.com/media/files/yolov3.weights
 wget -O ./models/yolov3-tiny.weights https://pjreddie.com/media/files/yolov3-tiny.weights
+wget -O ./cfg/yolov4-tiny.cfg https://github.com/AlexeyAB/darknet/raw/master/cfg/yolov4-tiny.cfg
+wget -O ./models/yolov4-tiny.weights https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov4-tiny.weights
